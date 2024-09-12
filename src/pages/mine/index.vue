4636f2614e5501ab2375b893c685ed83d20095cf
FaseuHard <l5713977886@outlook.com>
2024-09-13 02:10:21 +0800

    init2

--- src/pages/mine/index.vue
+++ src/pages/mine/index.vue
@@ -25,9 +25,6 @@
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    width: 100vw;
-    height: 100vh;
-    background: linear-gradient(90deg, #0e1e34, #0d1522);
   }
 
   .logo {
